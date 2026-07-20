c9a0aab7141ea6ca83c770d41fee13a27a648c63
Leonardo de Oliveira <leonardo93_tech@outlook.com>
2026-07-20 08:35:27 -0300

    Criando uma função no codigo

--- Exercicios/exercicio11.py
+++ Exercicios/exercicio11.py
@@ -10,44 +10,47 @@ def linha():
     print('\033[1;34m=-\033[m'*30)
 
 
-while True:
-    jogadores['nome'] = input('Nome do Jogador: ').title()
-    partida = int(input(f'Quantas partidas {jogadores["nome"]} jogou: '))
+def funcao_jogadores():
 
-    for contador in range(1, partida + 1):
-        gols = int(input(f'Quantos Gols na Partida {contador}: '))
-        gols_lista.append(gols)
+    while True:
+        jogadores['nome'] = input('Nome do Jogador: ').title()
+        partida = int(input(f'Quantas partidas {jogadores["nome"]} jogou: '))
 
-    jogadores['gols'] = gols_lista.copy()
-    jogadores['total'] = sum(gols_lista)
+        for contador in range(1, partida + 1):
+            gols = int(input(f'Quantos Gols na Partida {contador}: '))
+            gols_lista.append(gols)
+
+        jogadores['gols'] = gols_lista.copy()
+        jogadores['total'] = sum(gols_lista)
+
+        linha()
+
+        time.append(jogadores.copy())
+        gols_lista.clear()
+
+        continuar = str(input('Quer Continuar [S/N]: ')).lower()[0]
+
+    # os.system limpa a tela para digitar o proximo jogador
+        system('clear')
+        if continuar == 'n':
+            break
+    print()
 
     linha()
 
-    time.append(jogadores.copy())
-    gols_lista.clear()
+    # Jogador que fez mais gols 
+    print('\t-- ARTILHEIRO DO CAMPEONATO --')
 
-    continuar = str(input('Quer Continuar [S/N]: ')).lower()[0]
+    # Esta funcao lambda tras o jogador que fez mais gols
+    artilheiro = max(time, key=lambda jogador: jogador['total'])
+    print(artilheiro)
 
-# os.system limpa a tela para digitar o proximo jogador
-    system('clear')
-    if continuar == 'n':
-        break
-print()
-
-linha()
-
-# Jogador que fez mais gols 
-print('\t-- ARTILHEIRO DO CAMPEONATO --')
-
-# Esta funcao lambda tras o jogador que fez mais gols
-artilheiro = max(time, key=lambda jogador: jogador['total'])
-print(artilheiro)
-
-linha()
-print(f'{"cod nome":<25} {"gols":<20} {"total":<15}')
-for chave, valor in enumerate(time):
-    print(f'{chave:<3} {valor["nome"]:<20} {str(valor["gols"]):<22} {valor["total"]}')
+    linha()
+    print(f'{"cod nome":<25} {"gols":<20} {"total":<15}')
+    for chave, valor in enumerate(time):
+        print(f'{chave:<3} {valor["nome"]:<20} {str(valor["gols"]):<22} {valor["total"]}')
     
+funcao_jogadores()
 
 while True:
 
